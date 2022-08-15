6eb20b98db693dfbe3b01dd309d194613cf64534
Vesa Karvonen <vesa.a.j.k@gmail.com>
2022-08-16 01:52:34 +0300

    Remove duplication of Rea combinators

--- src/main/StdlibPlus/Misc.mli
+++ src/main/StdlibPlus/Misc.mli
@@ -1,5 +1,3 @@
-open Rea
-
 module Syntax : sig
   type 'a uop = 'a -> 'a
   type 'a bop = 'a -> 'a -> 'a
@@ -11,21 +9,6 @@ module Syntax : sig
   val eq'2 : ('a * 'b) bpr
   val eq'3 : ('a * 'b * 'c) bpr
   val eq'4 : ('a * 'b * 'c * 'd) bpr
-
-  (* *)
-
-  val pure'0 : (unit -> 'a) -> ('R, 'e, 'a, (('R, 'D) #pure' as 'D)) er
-  val pure'1 : ('b1 -> 'a) -> 'b1 -> ('R, 'e, 'a, (('R, 'D) #pure' as 'D)) er
-
-  val pure'2 :
-    ('b1 -> 'b2 -> 'a) -> 'b1 -> 'b2 -> ('R, 'e, 'a, (('R, 'D) #pure' as 'D)) er
-
-  val pure'3 :
-    ('b1 -> 'b2 -> 'b3 -> 'a) ->
-    'b1 ->
-    'b2 ->
-    'b3 ->
-    ('R, 'e, 'a, (('R, 'D) #pure' as 'D)) er
 
   (* *)
 
