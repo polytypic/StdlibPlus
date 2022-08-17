d9937acb8de44d382b71e674c753eb990559b98b
Vesa Karvonen <vesa.a.j.k@gmail.com>
2022-08-17 08:53:32 +0300

    Remove Uutf dependency

--- src/main/StdlibPlus/JsonString.ml
+++ src/main/StdlibPlus/JsonString.ml
@@ -14,8 +14,7 @@ let to_utf8_json = Fun.id
 
 let of_utf8 str =
   let buffer = Buffer.create (String.length str * 2) in
-  let encoder = Uutf.encoder `UTF_8 @@ `Buffer buffer in
-  let encode c = Uutf.encode encoder @@ `Uchar c |> ignore in
+  let add_uchar = Buffer.add_utf_8_uchar buffer in
   let to_hex i =
     Uchar.of_int
       (i
@@ -23,41 +22,40 @@ let of_utf8 str =
       if 0 <= i && i <= 9 then Uchar.to_int (Uchar.of_char '0')
       else Uchar.to_int (Uchar.of_char 'a') - 10)
   in
-  encode @@ Uchar.of_char '"';
-  str
-  |> Uutf.String.fold_utf_8
-       (fun i _ -> function
-         | `Malformed _ ->
-           failwithf "Malformed UTF-8 in string at char index %d" i
-         | `Uchar u ->
-           let c = Uchar.to_int u in
-           if (0x0000 <= c && c <= 0x001f) || (0x007f <= c && c <= 0x009f) then (
-             encode @@ Uchar.of_char '\\';
-             if u = Uchar.of_char '\b' then encode @@ Uchar.of_char 'b'
-             else if u = Uchar.of_int 0x0c then encode @@ Uchar.of_char 'f'
-             else if u = Uchar.of_char '\n' then encode @@ Uchar.of_char 'n'
-             else if u = Uchar.of_char '\r' then encode @@ Uchar.of_char 'r'
-             else if u = Uchar.of_char '\t' then encode @@ Uchar.of_char 't'
-             else (
-               encode @@ Uchar.of_char 'u';
-               encode @@ to_hex ((c lsr 12) land 0xf);
-               encode @@ to_hex ((c lsr 8) land 0xf);
-               encode @@ to_hex ((c lsr 4) land 0xf);
-               encode @@ to_hex ((c lsr 0) land 0xf)))
-           else if Uchar.of_char '"' = u || Uchar.of_char '\\' = u then (
-             encode @@ Uchar.of_char '\\';
-             encode u)
-           else encode u;
-           i + 1)
-       0
-  |> ignore;
-  encode @@ Uchar.of_char '"';
-  Uutf.encode encoder `End |> ignore;
+  add_uchar @@ Uchar.of_char '"';
+  let rec loop i =
+    if i < String.length str then
+      let decode = String.get_utf_8_uchar str i in
+      if Uchar.utf_decode_is_valid decode then (
+        let u = Uchar.utf_decode_uchar decode in
+        let c = Uchar.to_int u in
+        if (0x0000 <= c && c <= 0x001f) || (0x007f <= c && c <= 0x009f) then (
+          add_uchar @@ Uchar.of_char '\\';
+          if u = Uchar.of_char '\b' then add_uchar @@ Uchar.of_char 'b'
+          else if u = Uchar.of_int 0x0c then add_uchar @@ Uchar.of_char 'f'
+          else if u = Uchar.of_char '\n' then add_uchar @@ Uchar.of_char 'n'
+          else if u = Uchar.of_char '\r' then add_uchar @@ Uchar.of_char 'r'
+          else if u = Uchar.of_char '\t' then add_uchar @@ Uchar.of_char 't'
+          else (
+            add_uchar @@ Uchar.of_char 'u';
+            add_uchar @@ to_hex ((c lsr 12) land 0xf);
+            add_uchar @@ to_hex ((c lsr 8) land 0xf);
+            add_uchar @@ to_hex ((c lsr 4) land 0xf);
+            add_uchar @@ to_hex ((c lsr 0) land 0xf)))
+        else if Uchar.of_char '"' = u || Uchar.of_char '\\' = u then (
+          add_uchar @@ Uchar.of_char '\\';
+          add_uchar u)
+        else add_uchar u;
+        loop (i + Uchar.utf_decode_length decode))
+      else failwithf "Malformed UTF-8 in string at char index %d" i
+  in
+  loop 0;
+  add_uchar @@ Uchar.of_char '"';
   Buffer.contents buffer
 
 let to_utf8 lit =
   let buffer = Buffer.create (String.length lit * 2) in
-  let encoder = Uutf.encoder `UTF_8 @@ `Buffer buffer in
+  let add_uchar = Buffer.add_utf_8_uchar buffer in
   let hex_to_int h c =
     (h lsl 4)
     lor
@@ -73,45 +71,45 @@ let to_utf8 lit =
     || Uchar.of_char '\n' = c
     || Uchar.of_char '\r' = c
   in
-  lit
-  |> Uutf.String.fold_utf_8
-       (fun (s, i) _ u ->
-         let encode c =
-           Uutf.encode encoder @@ `Uchar c |> ignore;
-           (`Unescaped, i + 1)
-         in
-         match (s, u) with
-         | `Unescaped, `Uchar c ->
-           if Uchar.of_char '\\' = c then (`Escaped, i + 1)
-           else if Uchar.of_char '"' = c then (`Unescaped, i + 1)
-           else if Uchar.of_char '\n' = c || Uchar.of_char '\r' = c then (
-             Uutf.encode encoder @@ `Uchar (Uchar.of_char '\n') |> ignore;
-             (`Escaped, i + 1))
-           else encode c
-         | `Escaped, `Uchar c ->
-           if
-             Uchar.of_char '"' = c
-             || Uchar.of_char '\\' = c
-             || Uchar.of_char '/' = c
-           then encode c
-           else if Uchar.of_char 'b' = c then encode (Uchar.of_char '\b')
-           else if Uchar.of_char 'f' = c then encode (Uchar.of_int 0x0c)
-           else if Uchar.of_char 'n' = c then encode (Uchar.of_char '\n')
-           else if Uchar.of_char 'r' = c then encode (Uchar.of_char '\r')
-           else if Uchar.of_char 't' = c then encode (Uchar.of_char '\t')
-           else if is_white c then (`Continued, i + 1)
-           else (`Hex0, i + 1)
-         | `Continued, `Uchar c ->
-           ((if is_white c then `Continued else `Unescaped), i + 1)
-         | `Hex0, `Uchar c -> (`Hex1 (hex_to_int 0 c), i + 1)
-         | `Hex1 h, `Uchar c -> (`Hex2 (hex_to_int h c), i + 1)
-         | `Hex2 h, `Uchar c -> (`Hex3 (hex_to_int h c), i + 1)
-         | `Hex3 h, `Uchar c -> encode (Uchar.of_int (hex_to_int h c))
-         | _, `Malformed _ ->
-           failwithf "Malformed UTF-8 in string literal at char index %d" i)
-       (`Unescaped, 0)
-  |> ignore;
-  Uutf.encode encoder `End |> ignore;
+  let rec loop i s =
+    if i < String.length lit then
+      let decode = String.get_utf_8_uchar lit i in
+      if Uchar.utf_decode_is_valid decode then
+        let c = Uchar.utf_decode_uchar decode in
+        let i = i + Uchar.utf_decode_length decode in
+        let encode c =
+          add_uchar c;
+          loop i `Unescaped
+        in
+        match s with
+        | `Unescaped ->
+          if Uchar.of_char '\\' = c then loop i `Escaped
+          else if Uchar.of_char '"' = c then loop i `Unescaped
+          else if Uchar.of_char '\n' = c || Uchar.of_char '\r' = c then (
+            add_uchar (Uchar.of_char '\n');
+            loop i `Escaped)
+          else encode c
+        | `Escaped ->
+          if
+            Uchar.of_char '"' = c
+            || Uchar.of_char '\\' = c
+            || Uchar.of_char '/' = c
+          then encode c
+          else if Uchar.of_char 'b' = c then encode (Uchar.of_char '\b')
+          else if Uchar.of_char 'f' = c then encode (Uchar.of_int 0x0c)
+          else if Uchar.of_char 'n' = c then encode (Uchar.of_char '\n')
+          else if Uchar.of_char 'r' = c then encode (Uchar.of_char '\r')
+          else if Uchar.of_char 't' = c then encode (Uchar.of_char '\t')
+          else if is_white c then loop i `Continued
+          else loop i `Hex0
+        | `Continued -> loop i (if is_white c then `Continued else `Unescaped)
+        | `Hex0 -> loop i @@ `Hex1 (hex_to_int 0 c)
+        | `Hex1 h -> loop i @@ `Hex2 (hex_to_int h c)
+        | `Hex2 h -> loop i @@ `Hex3 (hex_to_int h c)
+        | `Hex3 h -> encode (Uchar.of_int (hex_to_int h c))
+      else failwithf "Malformed UTF-8 in string literal at char index %d" i
+  in
+  loop 0 `Unescaped;
   Buffer.contents buffer
 
 (* *)
--- src/main/StdlibPlus/UTF.ml
+++ src/main/StdlibPlus/UTF.ml
@@ -1,45 +1,31 @@
 open Exn.Syntax
+open Fun.Syntax
 
-let encode_as encoding chars =
+let encode_with add_uchar chars =
   let buffer = Buffer.create (Array.length chars * 2) in
-  let encoder = Uutf.encoder encoding @@ `Buffer buffer in
-  (chars
-  |> Array.iter @@ fun char -> ignore @@ Uutf.encode encoder @@ `Uchar char);
-  Uutf.encode encoder `End |> ignore;
+  Array.iter (add_uchar buffer) chars;
   buffer
 
-module UTF8 = struct
-  let of_uchar_array chars = chars |> encode_as `UTF_8 |> Buffer.contents
+let decode_with m length get input =
+  let buffer = Array.make (length input) Uchar.min in
+  let rec loop i j =
+    if i < length input then
+      let decode = get input i in
+      if Uchar.utf_decode_is_valid decode then (
+        buffer.(j) <- Uchar.utf_decode_uchar decode;
+        loop (i + Uchar.utf_decode_length decode) (j + 1))
+      else failwithf "Malformed UTF-%s at char index %d" m i
+    else j
+  in
+  let n = loop 0 0 in
+  if n <> Array.length buffer then Array.sub buffer 0 n else buffer
 
-  let to_uchar_array str =
-    let buffer = Array.make (Stdlib.String.length str) Uchar.min in
-    let n =
-      str
-      |> Uutf.String.fold_utf_8
-           (fun i _ -> function
-             | `Uchar c ->
-               buffer.(i) <- c;
-               i + 1
-             | `Malformed _ -> failwithf "Malformed UTF-8 at char index %d" i)
-           0
-    in
-    if n <> Array.length buffer then Array.sub buffer 0 n else buffer
+module UTF8 = struct
+  let of_uchar_array = encode_with Buffer.add_utf_8_uchar >>> Buffer.contents
+  let to_uchar_array = decode_with "8" String.length String.get_utf_8_uchar
 end
 
 module UTF16 = struct
-  let of_uchar_array chars = chars |> encode_as `UTF_16 |> Buffer.to_bytes
-
-  let to_uchar_array bytes =
-    let buffer = Array.make (Bytes.length bytes / 2) Uchar.min in
-    let n =
-      bytes |> Bytes.to_string
-      |> Uutf.String.fold_utf_16be
-           (fun i _ -> function
-             | `Uchar c ->
-               buffer.(i) <- c;
-               i + 1
-             | `Malformed _ -> failwithf "Malformed UTF-16 at char index %d" i)
-           0
-    in
-    if n <> Array.length buffer then Array.sub buffer 0 n else buffer
+  let of_uchar_array = encode_with Buffer.add_utf_16be_uchar >>> Buffer.to_bytes
+  let to_uchar_array = decode_with "16" Bytes.length Bytes.get_utf_16be_uchar
 end
